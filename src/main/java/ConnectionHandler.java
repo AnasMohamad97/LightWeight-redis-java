3af7805f4a10e6fc315f5f9d2cd79babdb6fbf10
SobhyA1 <anas.sobhy@vodafone.com.eg>
2025-11-16 12:24:15 +0200

    multiple connection handling

---
+++ src/main/java/ConnectionHandler.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+
+public class ConnectionHandler implements Runnable {
+    private final Socket client ;
+
+
+    public  ConnectionHandler(Socket client) throws IOException {
+        this.client=client;
+      }
+    @Override
+    public void run() {
+        // flushes after every print call
+        PrintWriter out = null;
+
+        BufferedReader in = null;
+        // buffered reader automatically handle line breaks
+
+        try {
+            out = new PrintWriter(client.getOutputStream(), true , StandardCharsets.UTF_8);
+            in = new BufferedReader(new InputStreamReader(client.getInputStream(), StandardCharsets.UTF_8));
+            String inputLine;
+            while ((inputLine = in.readLine()) != null) {
+                if("PING".equals(inputLine)){
+                    out.println("+PONG\r\n");
+                    out.flush();
+                }
+            }
+        }catch (IOException e){
+            System.out.println("From Socket: "+this.client.getInetAddress()+":"+this.client.getLocalPort() + " IOException " + e.getMessage());
+        }
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,8 +2,14 @@ import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Main {
+
+
   public static void main(String[] args){
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     System.out.println("Logs from your program will appear here!");
@@ -12,30 +18,22 @@ public class Main {
         ServerSocket serverSocket = null;
         Socket clientSocket = null;
         int port = 6379;
+
+        ExecutorService threadPool = Executors.newCachedThreadPool();
         try {
           serverSocket = new ServerSocket(port);
-          // Since the tester restarts your program quite often, setting SO_REUSEADDR
-          // ensures that we don't run into 'Address already in use' errors
           serverSocket.setReuseAddress(true);
-          // Wait for connection from client.
-          clientSocket = serverSocket.accept();
-          BufferedReader reader =new BufferedReader(new InputStreamReader(clientSocket.getInputStream())); // read from socket
-          BufferedWriter writer =new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream())); // write to the socket
-          while(true){
-              new Thread(()->{
-                  try {
-                      if(reader.readLine().equals("PING")){
-                          writer.write("+PONG\r\n");
-                          writer.flush();
-                      }
-                  } catch (IOException e) {
-                      throw new RuntimeException(e);
-                  }
-              });
+
+        } catch (IOException e) {
+          System.out.println("IOException: " + e.getMessage());
+        } try {
+          while (!Thread.currentThread().isInterrupted()) {
+              clientSocket = serverSocket.accept(); // connect to the server
+              threadPool.submit(new ConnectionHandler(clientSocket));
           }
         } catch (IOException e) {
           System.out.println("IOException: " + e.getMessage());
-        } finally {
+      } finally {
           try {
             if (clientSocket != null) {
               clientSocket.close();
